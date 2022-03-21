761d917bf5d490232cdaf5254279e4d565df6a13
jhoh <jaewhan0995@naver.com>
2022-03-21 23:50:45 +0900

    [FEAT] back testing script

--- back_testing.py
+++ back_testing.py
@@ -3,8 +3,20 @@ import os
 import time
 
 import pyupbit
+import schedule
 
 from inference import predict_arima, predict_prophet
+
+# login
+# access = "6zx2diWSu3Ad7I5z9maXjCYT7x5xZWGtnI4U3mtO"          # personal value
+# secret = "e5Hp9qfxVCULo65ysIHxXFDy5ui9IEOxUCLSxK0j"          # personal value
+
+# upbit = pyupbit.Upbit(access, secret)
+print("autotrade start")
+
+# BTC / ETH / LINK
+ticker = "KRW-BTC"
+tic = "BTC"
 
 
 def get_start_time(ticker):
@@ -14,52 +26,71 @@ def get_start_time(ticker):
     return start_time
 
 
+def get_balance(ticker):
+    ''' get balance '''
+    balances = upbit.get_balance()
+    print(balances)
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['balance'] is not None:
+                return float(b['balance'])
+            else:
+                return 0
+
+
 def get_current_price(ticker):
     ''' current price '''
-    return pyupbit.get_orderbook(ticker=ticker)['orderbook_units'][0]['ask_price']
+    return pyupbit.get_orderbook(tickers=ticker)[0]['orderbook_units'][0]['ask_price']
 
 
-# BTC / ETH / LINK
-ticker = "KRW-BTC"
-tic = "BTC"
-
-now = datetime.datetime.now()
-start_time = get_start_time(ticker)
-end_time = start_time + datetime.timedelta(hours=12)
-
-current_price = get_current_price(ticker)
-print(current_price)
+future_price = 0
+def predict_price(ticker):
+    '''prediction future price'''
+    global future_price
+    df = pyupbit.get_ohlcv(ticker, interval='minute15', count=1000)
 
 
-'''
+    future_price = 0
+predict_price(ticker)
+
+# predict future price every 15 minutes
+schedule.every(15).minutes.do(lambda: predict_price(ticker))
+
 # start auto trade
+print("autotrade start!!")
 while True:
     try:
         now = datetime.datetime.now()
         start_time = get_start_time(ticker)
         end_time = start_time + datetime.timedelta(hours=12)
+        schedule.run_pending()
 
         # 09:00 ~ 21:00
-        if start_time < now < end_time:
-            # training model
-
-
-            # predict_price
-            predict_price = inference(ticker)
+        if start_time < now < end_time - datetime.timedelta(seconds=10):
             current_price = get_current_price(ticker)
 
-            if predict_price < current_price:
-                btc = upbit.get_balance(tic)
-                if btc > 0.00008:
-                    upbit.sell_market_order(ticker, btc*0.9995) # 수수료 0.05 고려
-            else:
+            # if future price is higher than current price, buy the coin
+            if future_price > current_price:
                 krw = upbit.get_balance("KRW")
-                if krw > 5000: # 최소 주문 가능 금액
-                    upbit.buy_market_order(ticker, krw*0.9995) # 수수료 0.05 고려
+                if krw > 1000: # 최소 주문 가능 금액
+                    upbit.buy_marker_order(ticker, krw*0.9995) # 수수료 0.05%
+            
+            # if future price is lower than current price, pass this time
+            else:
+                pass
+            
+            # selling time
+            if now.minute % 15 == 0:
+                btc = upbit.get_balance(tic)
+                if btc > 0.00001:
+                    upbit.sell_market_order(ticker, btc*0.9995) # 수수료 0.05 고려
         else:
-            # os.system("shutdown -s -f")
             break
+        
+        # wait 1 minute
+        time.sleep(1)
+
     except Exception as e:
         print(e)
-        break
-'''
+        time.sleep(1)
+
